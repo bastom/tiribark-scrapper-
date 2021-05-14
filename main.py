b15499548276fa78e4708af890fa150d5ff6b91e
Achraf Ameddah <fm_ameddah@esi.dz>
2021-05-14 22:59:36 +0200

    [Update] the working dataset

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests as requests
 from selenium import webdriver
 import time
 
-f = open('./man.json', 'r')
+f = open('./woman.json', 'r')
 json_data = json.loads(f.readlines()[0])
 
 driver = webdriver.Chrome("./chromedriver")
@@ -18,28 +18,27 @@ for j in range(len(json_data)):
         driver.execute_script("document.querySelectorAll('[data-var=acceptBtn]')[0].click();")
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight/2);")
     time.sleep(3)
+    try:
+        elem = driver.find_element_by_id("pdp-6-up")
+        buttons = driver.find_elements_by_xpath('//button[@data-sub-type="image"]')
 
-    elem = driver.find_element_by_id("pdp-6-up")
-    buttons = driver.find_elements_by_xpath('//button[@data-sub-type="image"]')
+        for i in range(len(buttons)):
+            pictureTag = buttons[i].find_element_by_tag_name('div').find_elements_by_tag_name('picture')[1]
+            image_url = pictureTag.find_elements_by_tag_name('img')[0].get_attribute("src")
+            if image_url is not None:
+                r = requests.get(image_url)
 
-    for i in range(len(buttons)):
-        pictureTag = buttons[i].find_element_by_tag_name('div').find_elements_by_tag_name('picture')[1]
-        image_url = pictureTag.find_elements_by_tag_name('img')[0].get_attribute("src")
-        if image_url is not None:
-            r = requests.get(image_url)
+                # Check if the image was retrieved successfully
+                if r.status_code == 200:
+                    # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+                    r.raw.decode_content = True
 
-            # Check if the image was retrieved successfully
-            if r.status_code == 200:
-                # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
-                r.raw.decode_content = True
-
-                # Open a local file with write binary permission.
-                with open('./'+json_data[j]['id']+'/'+json_data[1]['id'] + '-' + str(i) + '.png', 'wb') as f:
-                    for chunk in r:
-                        f.write(chunk)
-            else:
-                print('Image Couldn\'t be retreived')
-
+                    # Open a local file with write binary permission.
+                    with open('./'+json_data[j]['id']+'/'+json_data[1]['id'] + '-' + str(i) + '.png', 'wb') as f:
+                        for chunk in r:
+                            f.write(chunk)
+                else:
+                    print('Image Couldn\'t be retreived')
+    except:
+        print("error in link", link)
 driver.close()
-if __name__ == "__main__":
-    print('tartour')
